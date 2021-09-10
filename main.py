0d75c6fca7b7d89dfd612da6c247cf05afe3959c
kaoulis <kyriakoschiotis@gmail.com>
2021-09-10 17:26:18 +0300

    Final Version

--- main.py
+++ main.py
@@ -22,18 +22,10 @@ searches_path = os.path.join(basedir, 'data/spitogatos_data_engineer_assignment_
 rootdir = Path(searches_path)
 file_list = [f for f in rootdir.glob('**/*') if f.is_file()]
 
-## Alt Option1: Very very slow implementation... But skips json parsing errors
-# with open(file_list[0], 'r') as fl:
-#     lines = fl.readlines()
-#     for l in lines:
-#         search = ast.literal_eval(l)
-#         df = df.append(search, ignore_index=True)
-#     df = df.append(search, ignore_index=True)
-
-## Alt Option2: For previous pandas versions that did not support json lines
-# jsonfile1 = [json.loads(line.replace("'", '"').replace("False", "false").replace("True", "true")) for line in open(test_path, 'r')]
-
-## Load and merge Searches data in Pandas dataframe
+# ##########################
+## Cleaning      ###########
+# ##########################
+# Load and merge Searches data in a Pandas dataframe
 searches_df = None
 for f in file_list:
     with open(f, 'r') as fl:
@@ -41,14 +33,32 @@ for f in file_list:
         curr_df = pd.read_json(json_file, lines=True)
         searches_df = pd.concat([searches_df, curr_df], ignore_index=True)
 
-##
+## The property searches performed by real estate brokers need to be filtered out of the cleaned dataset.
+searches_df = searches_df[searches_df['brokerID'].isna()]
+searches_df = searches_df[searches_df['publishedBrokerSite'].isna()]
+
+## The columns that need to be kept in the cleaned dataset:
 filters = ['date', 'areaIDs', 'category', 'listingType', 'livingAreaLow', 'livingAreaHigh', 'priceLow', 'priceHigh',
            'newDevelopment', 'garage', 'storage', 'balcony', 'secureDoor', 'alarm', 'fireplace', 'elevator', 'garden',
            'roomsLow', 'roomsHigh', 'petsAllowed']
 
 searches_df = searches_df[filters]
 
-## Load Geo data in Pandas dataframe
+## Replacing default nan default values for priceLow, priceHigh, livingAreaLow and livingAreaHigh
+searches_df['priceLow'] = searches_df['priceLow'].replace(9, np.NaN)
+searches_df['priceLow'] = searches_df['priceLow'].replace(998, np.NaN)
+searches_df['priceHigh'] = searches_df['priceHigh'].replace(999999, np.NaN)
+searches_df['priceHigh'] = searches_df['priceHigh'].replace(99999999, np.NaN)
+searches_df['livingAreaLow'] = searches_df['priceHigh'].replace(3, np.NaN)
+searches_df['livingAreaHigh'] = searches_df['priceHigh'].replace(99999999, np.NaN)
+
+## Exporting the clean dataset
+searches_df.to_csv('cleaned_data.csv', index=False)
+
+# ##########################
+## Enriching     ###########
+# ##########################
+# Load Geo data in Pandas dataframe
 con = sqlite3.connect(geo_path)
 cur = con.cursor()
 
@@ -79,10 +89,9 @@ def getRegionID(geo_id):
 geo_df['countryId'] = geo_df.geographyId.map(getCountryID)
 geo_df['regionId'] = geo_df.geographyId.map(getRegionID)
 
-## Merging and finalising the geo_df
+## Merging and finalising the geo_df with country name and region name.
 country_df = geo_df[geo_df['country_flag'] == 1][['geographyId', 'name']]
 region_df = geo_df[geo_df['region_flag'] == 1][['geographyId', 'name']]
-# country_df.iloc[1]['name']
 
 geo_df = pd.merge(geo_df, country_df, how="left", left_on="countryId", right_index=True)
 geo_df = pd.merge(geo_df, region_df, how="left", left_on="regionId", right_index=True)
@@ -100,14 +109,38 @@ test_df = pd.merge(test_df, geo_df, how="left", left_on="areaIDs", right_index=T
 test_df.drop(['geographyId', 'level', 'parentId', 'region_flag', 'country_flag', 'countryId', 'regionId'],
              axis=1, inplace=True)
 
-# Dropping searches with null countryName because analysis of the data is going to be performed on a per country basis.
-test_df = test_df[test_df['countryName'].notna()]
+# # Dropping searches with null countryName because analysis of the data is going to be performed on a per country basis.
+# test_df = test_df[test_df['countryName'].notna()]
 
 test_df.insert(loc=0, column='search_id', value=test_df.index)
 
-##
+## Based on specifications we can encapsulate areaIDs and geographyNames in a list object minimizing scanned data
+# for the queries.
 res = test_df.groupby(['date', 'search_id', 'countryName', 'regionName', 'category', 'listingType', 'livingAreaLow',
                        'livingAreaHigh', 'priceLow', 'priceHigh', 'newDevelopment', 'garage', 'storage', 'balcony',
                        'secureDoor', 'alarm', 'fireplace', 'elevator', 'garden',
                        'roomsLow', 'roomsHigh', 'petsAllowed'], as_index=False, dropna=False)[
     ['areaIDs', 'geographyName']].aggregate(lambda x: list(x))
+
+## Exporting the enriched dataset
+res.to_csv('enriched_data.csv', index=False)
+
+# ##########################
+## Analysis      ###########
+# ##########################
+
+res.groupby(['countryName', 'category', 'listingType'], as_index=False, dropna=False).size().sort_values(
+    by=['countryName', 'size'], ascending=[True, False])
+##
+res.groupby(['countryName', 'regionName'], as_index=False, dropna=False).size().sort_values(
+    by=['countryName', 'size'], ascending=[True, False])
+
+##
+res[["countryName", "livingAreaLow", "livingAreaHigh", 'priceLow', 'priceHigh', 'roomsLow', 'roomsHigh']].groupby(
+    ['countryName']).describe()
+
+##
+res[["countryName", "category", "listingType", "newDevelopment", "garage", "storage",
+     "balcony", 'secureDoor', 'alarm', 'fireplace', 'elevator', 'garden']].groupby(['countryName']).describe()
+
+
